e5cd14dc17dfad5643bec33517da273d98b5892a
Paul <paul.voicu@dev.asc.de>
2020-05-21 14:33:46 +0300

    Added jump.js

--- app/MyDBReader.php
+++ app/MyDBReader.php
@@ -2,7 +2,9 @@
 
 namespace App;
 
+use HaydenPierce\ClassFinder\ClassFinder;
 use ReflectionClass;
+
 
 class MyDBReader
 {
@@ -15,10 +17,20 @@ class MyDBReader
 
     public function printProperties()
     {
-        $test = __NAMESPACE__ . '\\Models\\Post';
-        $reflect = new ReflectionClass(__NAMESPACE__ . '\\Models\\Post');
-        $fillableProperty = $reflect->getProperty('fillable');
-        $fillableProperty->setAccessible(true);
-        return $fillableProperty->getValue(new $test);
+//        $test = __NAMESPACE__ . '\\Models\\Post';
+//        $reflect = new ReflectionClass(__NAMESPACE__ . '\\Models\\Post');
+//        $fillableProperty = $reflect->getProperty('fillable');
+//        $fillableProperty->setAccessible(true);
+//        return $fillableProperty->getValue(new $test);
+//        $testResult = array();
+        $resultArray = array();
+        $classes = ClassFinder::getClassesInNamespace('App\\Models');
+        foreach ($classes as $myClass) {
+            $reflect = new ReflectionClass($myClass);
+            $fillableProperty = $reflect->getProperty('fillable');
+            $fillableProperty->setAccessible(true);
+            $resultArray[$myClass]  = $fillableProperty->getValue(new $myClass);
+        }
+        return $resultArray;
     }
 }
